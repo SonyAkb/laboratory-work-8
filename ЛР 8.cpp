c9568c0a96a00a4a61ced294959843ff7390ea78
SonyAkb <145361459+SonyAkb@users.noreply.github.com>
2024-02-21 16:23:25 +0500

    Создание структуры
    Создание структуры студент, добавление в структуру нового элемента

---
+++ ЛР 8.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+
+
+struct student { //структура Студент
+	string name = "Иван"; //имя
+	string last_name = "Иванов"; //фамилия
+	string patronymic = "Иванович"; //отчество
+	string data = "01.01.2000"; //дата рождения в формате ДД.ММ.ГГГГ
+	string address = "Комсомольский проспект, 29"; //домашний адрес
+	int rating = 50; //рейтинг от 0 до 100
+
+	void enter_inform(string n, string l, string p, string d, string a, int r) { //ввод информации о студенте
+		name = n; //имя
+		last_name = l; //фамилия
+		patronymic = p; //отчество
+		data = d; //дата рождения в формате ДД.ММ.ГГГГ
+		address = a; //домашний адрес
+		rating = r; //рейтинг от 0 до 100
+	}
+	
+};
+
+void print(student N) { //вывожу параметры студента
+	cout << "Вывод студента: ";
+	cout << N.last_name << ' ' << N.name << ' ' << N.patronymic << ' ' << N.data << ' ' << N.address << ' ' << N.rating << endl;
+}
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	system("chcp 1251");
+	system("cls");
+
+	const int max_size = 50;
+	student mas_students[max_size]; //массив всех студентов
+	int population; //количество студентов
+	
+	string name_st, last_n_st, patr_st, data_st, addr_st;
+	int rat;
+
+	do {
+		cout << "Введите количество студентов (от 1 до " << max_size << "): ";
+		cin >> population; //количество студентов
+	} while (population<1 || population>max_size);
+
+	for (int i = 0; i < population; i++) { //ввожу данные всех студентов
+		cout << "Введите Фамилию студента: ";
+		cin >> last_n_st; //фамилия
+		cout << "Введите Имя студента: ";
+		cin >> name_st; //имя
+		cout << "Введите Отчество студента: ";
+		cin >> patr_st; //отчество
+		cout << "Введите дату рождения студента (в формате ДД.ММ.ГГГГ): ";
+		cin >> data_st; //дата рождения в формате ДД.ММ.ГГГГ
+		cout << "Введите домашний адрес студента: ";
+		cin.ignore();
+		getline(cin, addr_st); //домашний адрес
+
+		do {
+			cout << "Введите рейтинг студента: ";
+			cin >> rat; //рейтинг от 0 до 100
+		} while (rat < 0 || rat > 100);
+		cout << endl;
+
+		mas_students[i].enter_inform(name_st, last_n_st, patr_st, data_st, addr_st, rat);
+	}
+
+	for (int i = 0; i < population; i++) {
+		cout << "ВЫВОД имя студента: "<<i<<endl;
+		print(mas_students[i]);
+
+	}
+
+	//student N;
+	//N.enter_inform("Ivan", "Ivanov", "Ivanoviz", "01.12.2001", "Ekaterininskai, 4", 52);
+	
+	//print(N);
+
+
+	return 0;
+}
